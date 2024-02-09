b2ca24ce9b9de3e1f0bb5fb94419ac589e39c645
Vsevolod <114590209+porunit@users.noreply.github.com>
2024-02-09 19:25:22 +0300

    sfsdfdsgdklfg

--- src/main/java/com/porunit/l4/controllers/ShotController.java
+++ src/main/java/com/porunit/l4/controllers/ShotController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/hit")
+@CrossOrigin("*")
 public class ShotController {
 
     private final ShotService shotService;
